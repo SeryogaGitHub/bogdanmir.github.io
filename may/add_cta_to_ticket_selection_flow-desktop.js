aad66994320e1b55706d5dde3bec4b7ed6cf5e7f
bogdanmir <muronhtz@gmail.com>
2020-02-11 13:54:21 +0200

    Upd

--- may/add_cta_to_ticket_selection_flow-desktop.js
+++ may/add_cta_to_ticket_selection_flow-desktop.js
@@ -123,16 +123,17 @@ function readyjQueryinit(){
 			justify-content: space-between;
 		}
 	}
-	@media (max-width: 400px) {
-		.ab_show_fares_btn, .ab_select_btn {
-			padding: 10px 14px;
-			font-size: 8px;
-		}
-		.ab_show_fares_btn {
-			width: 130px;
-			margin-left: -30px;
-		}
-	}
+	// @media (max-width: 400px) {
+	// 	.ab_show_fares_btn,
+	// 	.ab_select_btn {
+	// 		padding: 10px 14px;
+	// 		font-size: 8px;
+	// 	}
+	// 	.ab_show_fares_btn {
+	// 		width: 130px;
+	// 		margin-left: -30px;
+	// 	}
+	// }
 	`;
 	styles    += "</style>";
 	jQuery('body').append(styles);
